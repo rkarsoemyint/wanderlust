81c217dd21f49063a6b42f6070517f8501d01f97
rkarsoemyint <tzoo2024.com>
2026-03-03 14:19:38 +0630

    Use CLOUDINARY_URL environment variable

--- wanderlust/settings.py
+++ wanderlust/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-iw_n+@$t!y0j2o$w0209%
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost, 127.0.0.1, wanderlust-o0p4.onrender.com']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
